a3e67348aed2b353e1b4fff18b946175bda69571
Graeme Gregory <graeme@xora.org.uk>
2017-02-22 23:07:41 +0000

    qdos.c : Add fullscreen and jobname
    Signed-off-by: Graeme Gregory <graeme@xora.org.uk>

---
+++ qdos.c
@@ -0,0 +1,14 @@
+#include <qdos.h>
+
+WINDOWDEF_t _condetails = {
+   2, /* border colour (red) */
+   0, /* border width */
+   0, /* paper (black) */
+   7, /* ink (white) */
+   512, /* width (pixel) */
+   256, /* height (pixels) */
+   0, /* x origin */
+   0 /* y origin */
+};
+
+char    _prog_name[] = "Wander";
